e54018d3066cd18b1ff49ad8f3bc13426edabf03
Kan Mengkhorn <35882942+Mengkhorn@users.noreply.github.com>
2019-10-03 15:05:56 +0700

    Update sample.php

--- sample.php
+++ sample.php
@@ -1,7 +1,7 @@
 //My code for get client desktop name
 $ipaddress= $_SERVER['REMOTE_ADDR'];
 $name = gethostbyaddr($ipaddress);
-echo name;
+echo $name;
 
 Resulte of is DESKTOP-ULGUDNE it is my computer name
 but it working only localhost when project in server it retuen ip like this 192.168.1.11 .......
